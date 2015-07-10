f8a1478606c40f26bca6c7867f173b6ad952aa44
ahmed onawale <ahmed.onawale@andela.co>
2015-07-10 08:47:18 +0100

    create a mongodb instance on mongolab

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/tides',
+	db: 'mongodb://ahmedonawale:pr0t0c0l@ds047742.mongolab.com:47742/tides-research',
 	assets: {
 		lib: {
 			css: [
