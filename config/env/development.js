9c9fccd60c23eb6288c59050078e02b2473050d1
ahmed onawale <ahmed.onawale@andela.co>
2015-07-10 11:26:15 +0100

    create a mongodb instance on mongolab

--- config/env/development.js
+++ config/env/development.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-	// db: 'mongodb://localhost/tides-dev',
-	db: 'mongodb://ammyreal:pr0t0c0l@ds047742.mongolab.com:47742/tides-research',
+	db: 'mongodb://localhost/tides-dev',
+	// db: 'mongodb://ammyreal:pr0t0c0l@ds047742.mongolab.com:47742/tides-research',
 	app: {
 		title: 'tides - Development Environment'
 	},
